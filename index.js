413e31afd01b63b4273b696662eeb574ec8c5cb3
MariaYminova <vabylia96@gmail.com>
2023-01-23 20:14:30 +0400

    added a slider to the popular section

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
- var swiper = new Swiper(".mySwiper", {
-        pagination: {
-          el: ".swiper-pagination",
-          dynamicBullets: true,
-        },
-      });
+const swiperBaner = new Swiper(".slider-baner", {
+  pagination: {
+    el: ".pagination-baner",
+
+  },
+});
+
+const swiperPop = new Swiper(".slider-popular", {
+  slidesPerView: 4,
+  spaceBetween: 18,
+
+  scrollbar: {
+    el: ".scrollbar-popular",
+    draggable: true,
+    dragSize: 143,
+  },
+
+
+
+
+
+});
