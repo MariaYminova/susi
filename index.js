f6cbc0e9f3e52bf306b720d6ae1222b4a477c324
MariaYminova <vabylia96@gmail.com>
2023-01-30 19:57:57 +0400

    added a menu page

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const swiperBaner = new Swiper(".slider-baner", {
   },
 });
 
+
 const swiperPop = new Swiper(".slider-popular", {
   slidesPerView: 4,
   spaceBetween: 18,
@@ -14,9 +15,43 @@ const swiperPop = new Swiper(".slider-popular", {
     draggable: true,
     dragSize: 143,
   },
-
-
-
-
-
 });
+
+const tabsBtn = document.querySelectorAll(".navigation__tab")
+const tabsitem = document.querySelectorAll(".slider-positions__slide")
+
+function tabsControl(currentBtn) {
+  const tabId = currentBtn.getAttribute("data-tab")
+  let currentTab = document.querySelector(tabId)
+
+  tabsBtn.forEach(function (item) {
+    item.classList.remove("active")
+  })
+
+  tabsitem.forEach(function (item) {
+    item.classList.remove("active")
+  })
+
+  currentBtn.classList.add("active")
+  currentTab.classList.add("active")
+}
+
+tabsBtn.forEach(function (tab) {
+  tab.addEventListener("click", function () {
+    tabsControl(this)
+
+    carretAnimate(this)
+  })
+})
+
+const swiperPosition = new Swiper(".slider-positions", {
+ 
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+});
+
+
+   
+
