da636fe1d18cae6df15b202af8a2eafeb396b521
MariaYminova <vabylia96@gmail.com>
2023-02-06 20:15:24 +0400

    added adaptive home page

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const swiperBaner = new Swiper(".slider-baner", {
   pagination: {
     el: ".pagination-baner",
-
   },
 });
 
 
 const swiperPop = new Swiper(".slider-popular", {
-  slidesPerView: 4,
+  slidesPerView: 'auto',
   spaceBetween: 18,
 
   scrollbar: {
@@ -44,14 +43,69 @@ tabsBtn.forEach(function (tab) {
   })
 })
 
-const swiperPosition = new Swiper(".slider-positions", {
- 
-  navigation: {
-    nextEl: ".next",
-    prevEl: ".prev",
-  },
-});
+const filterBtn = document.querySelectorAll(".button-transparent")
+
+filterBtn.forEach(item => {
+  item.addEventListener('click', (e) => {
+    filterBtn.forEach(el => { el.classList.remove('active'); })
+    item.classList.add('active')
+  })
+})
+
+const filterBtnIcon = document.querySelectorAll(".button-with-icon")
+
+filterBtnIcon.forEach(item => {
+  item.addEventListener('click', (e) => {
+    filterBtnIcon.forEach(el => { el.classList.remove('active'); })
+    item.classList.add('active')
+  })
+})
+
+const btnStar = document.querySelectorAll(".button-star")
+
+btnStar.forEach(item => {
+  item.addEventListener('click', (e) => {
+    item.classList.toggle('active');
+
+  })
+})
+
+const btnlike = document.querySelectorAll(".icon-like")
+
+btnlike.forEach(item => {
+  item.addEventListener('click', (e) => {
+    item.classList.toggle('active');
+
+  })
+})
 
 
-   
+
+
+const btnLike = document.querySelectorAll('.icon-like');
+
+let like = true,
+  likeCount = document.querySelector('.like-number').innerHTML;
+
+btnLike.forEach(item => {
+  item.addEventListener('click', () => {
+
+    likeCount = like ? ++likeCount : --likeCount;
+    like = !like;
+    document.querySelector('.like-number').innerHTML = likeCount;
+  });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
